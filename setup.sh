6f41c30b8b8520cccff36e58028ff97ef96a7a78
Bay-Yapay <166753316+Bay-Yapay@users.noreply.github.com>
2024-06-30 01:11:35 +0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sanal ortamı oluştur
+python3 -m venv venv
+source venv/bin/activate
+
+# Gerekli Python paketlerini yükle
+pip install -r requirements.txt
+
+# Diğer yapılandırma adımlarını buraya ekleyebilirsiniz
